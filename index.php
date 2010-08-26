cee98afd22fc8e83cff765508b60e4fa568680dc
Stefan Lange-Hegermann <lange-hegermann@sipgate.de>
2010-08-26 14:53:02 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="de">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Team Princess</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Stefan Lange-Hegermann">
+	<!-- Date: 2010-08-25 -->
+	
+	<style type="text/css" media="screen">
+		body {
+			font-family:Comic Sans MS;
+			background-color:#FFCCCC;
+			background-image:url(paranim.gif);
+			text-align:center;
+		}
+		h1 {
+			color:blue;
+		}
+		span.footer {
+			color:white;
+		}
+	</style>
+</head>
+<body>
+<h1><blink>Team Princess</blink></h1>
+<img src="princess.gif" alt=""><br>
+<img src="hevequip2.gif" width="413" height="100" alt="Hevequip2">
+<h2>Princess of the sprint:</h2>
+<img src="leya.jpg" alt="Pony Princess">
+<embed src="aschenbroedel.mid" autostart="true" loop="true"
+width="2" height="0">
+</embed>
+<br>
+<br>
+<br>
+<script language="Javascript" src="http://teamprincess.de/gcount12/graphcount.php?page=test"><!--
+//--></script>
+<br>
+<br>
+<img src="ie_button.gif"><br>
+
+<script type="text/javascript" charset="utf-8">
+	var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+	var on=true;
+	var header = document.getElementsByTagName('blink')[0];
+	window.setInterval(function () {
+		if (is_chrome) {
+			on=!on;
+			
+			if (on) {
+				header.style.opacity="0";
+			} else {
+				header.style.opacity="1";
+			}
+		
+		}
+	}, 1000);
+</script>
+
+</body>
+</html>
